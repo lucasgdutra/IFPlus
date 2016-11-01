a56658a01dd9b58698068ce2b65d0916a7820b7a
aluno3c <aluno3c@172.16.1.26>
2016-11-01 14:52:03 -0200

    mudand

--- header.php
+++ header.php
@@ -18,10 +18,10 @@
             <div class="collapse navbar-collapse" id="collapse-navbar" aria-expanded="false">
                 <div class="col-md-9">
                     <ul class="nav navbar-nav ">
-                        <li><a data-target="#" data-toggle="pill" href="../Agenda/index.php">Agenda</a></li>
-                        <li><a data-target="#" data-toggle="pill" href="../Caderno/index.php">Caderno</a></li>
-                        <li><a data-target="#" data-toggle="pill" href="../Chat/index.php">Chat</a></li>
-                        <li><a data-target="#" data-toggle="pill" href="../Compartilhador/index.php">Compartilhador de Arquivos</a></li>
+                        <li><a  href="../Agenda/index.php">Agenda</a></li>
+                        <li><a  href="../Caderno/index.php">Caderno</a></li>
+                        <li><a  href="../Chat/index.php">Chat</a></li>
+                        <li><a  href="../Compartilhador/index.php">Compartilhador de Arquivos</a></li>
                     </ul>
                 </div>
                 <div class="col-md-1">
@@ -32,26 +32,5 @@
             </div>
         </div>
     </nav>
-    <script>
-        /*menu handler*/
-        $(function () {
-            function stripTrailingSlash(str) {
-                if (str.substr(-1) == '/') {
-                    return str.substr(0, str.length - 1);
-                }
-                return str;
-            }
-
-            var url = window.location.pathname;
-            var activePage = stripTrailingSlash(url);
-
-            $('.nav li a').each(function () {
-                var currentPage = stripTrailingSlash($(this).attr('href'));
-
-                if (activePage == currentPage) {
-                    $(this).parent().addClass('active');
-                }
-            });
-        });</script>
 </header>
 
