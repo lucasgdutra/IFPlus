853d5a6edf27cde9287e341553b69bc486c4b3ef
matheussn <matheusneto23@gmail.com>
2016-11-03 09:58:21 -0200

    Alterações arquivos

---
+++ Arquivos/aluno.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>IFPlus - Arquivos</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="../bibliotecas/bootstrap/css/bootstrap.css">
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <?php $nome_tela="Arquivos"?>
+        <?php require '../header.php';?>
+        
+        <div class="input-group">
+            <input type="text" class="form-control" aria-label="...">
+            <div class="input-group-btn">
+                <botton type="button" class="btn btn-default" aria-label="Help">
+                    <span class="glyphicon glyphicon-question-sign"></span>
+                </botton>
+            </div>
+            
+        </div>
+
+        
+        
+        <script src="../bibliotecas/js/jquery.js"></script>
+        <script src="../bibliotecas/bootstrap/js/bootstrap.js"></script>
+    </body>
+</html>
