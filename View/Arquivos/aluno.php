8ad51ff958b38bc75720371ee6a6d3b2cabc281e
matheussn <matheusneto23@gmail.com>
2016-11-04 17:31:08 -0200

    alteração em Arquivos

--- View/Arquivos/aluno.php
+++ View/Arquivos/aluno.php
@@ -10,21 +10,14 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link type="text/css" rel="stylesheet" href="../bibliotecas/bootstrap/css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="../bibliotecas/css/style.css">   
         <style>
-            .navbar
-            {
-                margin-bottom: 3px;
+            .panel-default .panel-heading {
+                color: #000;
+                background-color: #fff;
             }
-            .nav li p
-            {
-                position: relative;
-                display: block;
-                padding: 10px 15px;
-            }
-            
-            p
-            {
-                margin: 0 0 0px;
+            .panel-body{
+               background-color: #f5f5f5;
             }
         </style>
     </head>
@@ -33,21 +26,26 @@ and open the template in the editor.
         <?php require '../header.php';?>
         
         <div class="container-fluid">
-            <div class="row" style="background-color: #fff">
-                <ul class="nav">
-                    <li class="active col-xs-12">
-                        <p class="text-center">Todos os Arquivos</p>
+            <div id="submenu" class="row">
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active col-xs-12">
+                        <a class="disabled">Todos os Arquivos</a>
                     </li>
                 </ul>
             </div>
             <br>
-            <div class="input-group">
-                <input type="text" class="form-control" aria-label="Pesquisar" placeholder="Pesquisar Arquivo">
-                <div class="input-group-btn">
-                    <botton type="button" class="btn btn-default" aria-label="Pesquisar">
-                        <span class="glyphicon glyphicon-search"></span>
-                    </botton>
-                </div> 
+            <div>
+                <div class="input-group">
+                    <input type="text" class="form-control" aria-label="Pesquisar" placeholder="Pesquisar Arquivo">
+                    <div class="input-group-btn">
+                        <botton type="button" class="btn btn-default" aria-label="Pesquisar">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </botton>
+                    </div>
+                </div>
+                
+                <?php require 'past.php'; ?>
             </div>
         </div>
         
