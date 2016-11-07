cb578ce4cc8a939b4398850f3b93623cfef2fe76
Lucas <Lucas@192.168.15.6>
2016-11-07 00:06:16 -0200

    novo index

--- View/header-initial.php
+++ View/header-initial.php
@@ -23,12 +23,7 @@
             <div class="row">
                 <div class="navbar-header col-md-6">
                     <div >
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-navbar" >
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
+                        
                         <a class="navbar-brand" href="#"><?php echo"IFPlus"; ?></a>
                     </div>
                 </div>
