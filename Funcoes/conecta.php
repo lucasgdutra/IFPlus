296d443315e966fde525767cd4eab21f9c3feefb
darley <darley@192.168.15.4>
2016-11-13 21:23:59 -0200

    renomeando funcoes

---
+++ Funcoes/conecta.php
@@ -0,0 +1,6 @@
+<?php
+$host = "mysql.hostinger.com.br";
+$user = "u244818129_dipol";
+$pass = "Olo()176";
+$database = "u743166944_ifplu";
+$conexao = mysqli_connect($host, $user, $pass, $database);
