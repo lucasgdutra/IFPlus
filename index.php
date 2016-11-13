947a039e726932e60e014e907d6c5647d0ba9d32
matheussn <matheusneto23@gmail.com>
2016-11-13 18:09:43 -0200

    colocando o direcionamento na index

--- index.php
+++ index.php
@@ -1,35 +1,10 @@
 <?php
-    //header("Location: ./View/login.php");
-?>
+    require './funcaomobile.php';
 
-<html>
-    <head>
-        <title>IFPlus</title>
-        <meta charset="utf-8"> 
-        <link type="text/css" rel="stylesheet" href="View//bibliotecas/css/reset.css">
-        <link type="text/css" rel="stylesheet" href="View/bibliotecas/bootstrap/css/bootstrap.css">
-        <link type="text/css" rel="stylesheet" href="View/bibliotecas/css/style.css">
-    </head>
-    <body>
-        
-        <header>
-            <h1>Olá... Temos aqui o visual da aplicação IFPlus para que você à veja.</h1>
-            <h4>Acesse os links abaixo para visualizar as telas das funcionalidades do app.</h4>
-        </header>
-        <div>
-            <a href="View/login-desktop.php">Tela de Login e cadastro - Desktop</a>
-            <br>
-            É aconselhavel que você acesse o link a baixo com um Smartphone, ou com a opção de visualizar como smartphone de seu navegador
-            <br>
-            <a href="View/login-smartphone.php">Tela de login - Smartphone</a>
-            <br>
-            <a href="View/Agenda/">Agenda</a>
-            <br>
-            <a href="View/Arquivos/">Arquivos</a>
-            <br>
-            <a href="View/Caderno/">Caderno</a>
-            <br>
-            <a href="View/Chat/">Chat</a>
-        </div>
-    </body>
-</html>
+    $mobile = mobile();
+    if ($mobile) {
+        header("Location: ./View/login-smartphone.php");
+    } else {
+        header("Location: ./View/login-desktop.php");
+    }
+    die();
