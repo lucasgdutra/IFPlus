8233197ea47c70ef31b74827be75cea60e4e2737
lucas <lucas@LGDS>
2016-11-21 23:28:54 -0200

    Teste de conexão com banco remoto

--- Funcoes/Login/banco-usuario.php
+++ Funcoes/Login/banco-usuario.php
@@ -6,19 +6,20 @@ require_once ROOT . 'Funcoes' . DS . 'conecta.php';
 function buscaUsuario($conexao, $email, $senha) {
     $senhaMd5 = md5($senha);
     $email = mysqli_real_escape_string($conexao, $email);
-    $query = "select * from usuario where email='{$email}' and senha='{$senha}'";
+    $query = "select * from Usuario where email='{$email}' and senha='{$senhaMd5}'";
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
     return $usuario;
 }
 
 function tipoUsuario($conexao, $id) {
-    $query = "select * from aluno where id_usuario='{$id}'";
+    $query = "select * from Aluno where id_usuario='{$id}'";
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
-    if($usuario == NULL){
+    if ($usuario == NULL) {
         $tipoUsuario = professor;
-    }else{
+    }
+    else {
         $tipoUsuario = aluno;
     }
     return $tipoUsuario;
