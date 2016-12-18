e1fc9d73781e7a68e58c519af4701859931a625b
Lucas Gabriel <darley@192.168.15.13>
2016-12-18 20:49:43 -0200

    *Mudando tabelas

--- Funcoes/Login/banco-usuario.php
+++ Funcoes/Login/banco-usuario.php
@@ -3,14 +3,14 @@
 function buscaUsuario($conexao, $email, $senha) {
     $senhaMd5 = md5($senha);
     $email = mysqli_real_escape_string($conexao, $email);
-    $query = "select * from Usuario where email='{$email}' and senha='{$senhaMd5}'";
+    $query = "select * from usuario where email='{$email}' and senha='{$senhaMd5}'";
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
     return $usuario;
 }
 
 function tipoUsuario($conexao, $id) {
-    $query = "select * from Professor WHERE id_usuario='{$id}'";
+    $query = "select * from professor WHERE id_usuario='{$id}'";
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
     if ($usuario == NULL) {
