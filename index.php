d631d0faccd5272f724b66ee1ff3b3ed67d79912
Lucas Gabriel <darley@192.168.15.13>
2016-12-18 16:49:38 -0200

    Recolocando index correto

--- index.php
+++ index.php
@@ -1,40 +1,22 @@
-<?php
-$nome_tela = "Agenda";
-require_once './config.php';
-require_once ROOT . "cabecalho.php";
-//require_once 'insert_db.php';
-verificaUsuario();
-require_once ROOT . 'View' . DS . 'navbar.php';
-?>
-<div id="alert">
-    <?php mostraAlerta("success"); ?>
-    <?php mostraAlerta("danger"); ?>
-    <?php mostraAlerta("info"); ?>
-    <?php mostraAlerta("warning"); ?>
-</div>
-<?php if (usuarioTipo() == "professor"): ?>
-    <div id="main" class="container-fluid">
-        <div class="row">
-            <div class="col-sm-9 col-md-10 no-padding">
-                <?php require_once ROOT . "View" . DS . "Agenda" . DS . "prof.php"; ?>
-            </div>
-            <div class = "col-sm-3 col-md-2 hidden-xs no-padding">
-                <?php require_once ROOT . "View" . DS . "Chat" . DS . 'barra-lateral.php'; ?>
-            </div>
-        </div>
-    </div>
-<?php endif; ?>
 
-<?php if (usuarioTipo() == "aluno"): ?>
-    <div id="main" class="container-fluid">
-        <div class="row">
-            <div class="col-sm-9 col-md-10 no-padding">
-                <?php require_once ROOT . "View" . DS . "Agenda" . DS . "aluno.php"; ?>
-            </div>
-            <div class = "col-sm-3 col-md-2 hidden-xs no-padding">
-                <?php require_once ROOT . "View" . DS . "Chat" . DS . "barra-lateral.php"; ?>
-            </div>
-        </div>
-    </div>
-<?php endif; ?>
-<?php require ROOT . 'rodape.php'; ?>
+<?php require_once 'config.php'; ?>
+<?php require_once ROOT . "cabecalho.php"; ?>
+<?php require_once ROOT . '/Funcoes/funcaomobile.php'; ?>
+
+<h1>Bem vindo!</h1>
+
+<?php
+if (usuarioEstaLogado()) {
+    header('Location:' . BASEURL . 'View/Agenda/index.php');
+} else {
+    $mobile = mobile();
+    if ($mobile) {
+        header('Location:' . BASEURL . 'View/login-smartphone.php');
+    } else {
+        header('Location:' . BASEURL . 'View/login-desktop.php');
+    }
+}
+?>
+<?php
+require_once("rodape.php");
+?>
