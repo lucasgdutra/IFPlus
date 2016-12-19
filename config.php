4eb3bc23c93aef19a78c62ca0c1f6b2552860f33
Lucas Gabriel <darley@192.168.15.13>
2016-12-19 04:52:17 -0200

    Pequenas correções de conflito

--- config.php
+++ config.php
@@ -1,17 +1,25 @@
 <?php
 
 error_reporting(E_ALL ^ E_NOTICE);
-define('DS', DIRECTORY_SEPARATOR);
+if (!defined('DS'))
+    define('DS', DIRECTORY_SEPARATOR);
+
 /** caminho absoluto para a pasta do sistema * */
-if (!defined('ROOT')) {
-    define('ROOT', __DIR__ . DS);
-}
+if (!defined('ROOT'))
+    define('ROOT', dirname(__FILE__) . '/');
 
 /** caminho no server para o sistema * */
-if (!defined('BASEURL')) {
-    //define('BASEURL', '/antigo/ifplus/');
+if (!defined('BASEURL'))
     define('BASEURL', '/ifplus/');
-}
+
+/** caminho do arquivo de banco de dados * */
+if (!defined('DBAPI'))
+    define('DBAPI', ROOT . 'Funcoes/Caderno/database.php');
+
+/** caminhos dos templates de header e footer * */
+define('HEADER_TEMPLATE', ROOT . 'cabecalho.php');
+define('FOOTER_TEMPLATE', ROOT . 'rodape.php');
+
 require_once ROOT . "Funcoes" . DS . "conecta.php";
 require_once ROOT . "mostra-alerta.php";
 require_once ROOT . 'Funcoes' . DS . 'Login' . DS . 'logica-usuario.php';
