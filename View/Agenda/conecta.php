fb4cde4eff9c3beb05cfcd673360f20d317be439
Marcos Antenor <marcosantenor86@gmail.com>
2016-12-18 23:10:05 -0200

    Add files via upload

---
+++ View/Agenda/conecta.php
@@ -0,0 +1,24 @@
+<?php
+
+//
+//$host = "mysql.hostinger.com.br";
+//$user = "u244818129_dipol";
+//$pass = "Olo()176";
+//$database = "u244818129_ifplu";
+//
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "u244818129_ifplu";
+
+function conexao($host, $user, $pass, $database) {
+    $conexao = mysqli_connect($host, $user, $pass, $database);
+// Verifica se ocorreu algum erro
+    if (mysqli_connect_error()) {
+        die("Não foi possível conectar-se ao banco de dados: " . utf8_encode(mysqli_connect_error()));
+        exit();
+    }
+    return $conexao;
+}
+
+$conexao = conexao($host, $user, $pass, $database);
